67f78437bc3175babf55b4dca8f7567a71036c95
Rosiel Victor <codepremium917@gmail.com>
2022-01-16 23:06:37 -0300

    Primeira versão da comunidade BBB 22

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { StyleSheet } from 'react-native';
+import { WebView } from 'react-native-webview'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <WebView 
+      style={styles.container}
+      source={{ uri: 'https://bbb.helpdevelop.top/' }}
+    />
   );
 }
 
@@ -17,5 +17,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop:20,
   },
 });
